8248e5baad727dcde5f3cb7c6d0cee4ce005d598
mshilpa54321 <mshilpa54321@gmail.com>
2023-03-09 15:56:10 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def Meghana(request):
+    return HttpResponse('My Name Is Meghanaa Sir')
+
